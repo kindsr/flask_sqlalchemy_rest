7dff73c772e8abe55a199d2e5f27778c4509dcfd
kindsr <kindsr82@gmail.com>
2020-11-09 09:44:32 +0900

    Initial commit

---
+++ test/main.py
@@ -0,0 +1,94 @@
+from flask import Flask #, request
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name = {name}, views = {views}, likes = {likes})"
+
+#db.create_all()
+
+#names = {"kindsr": {"age": 39, "gender": "male"},
+#         "tim":    {"age": 19, "gender": "male"}}
+
+#class HelloWorld(Resource):
+#    def get(self, name):
+#        return names[name]
+
+    #def get(self, name, test):
+    #    return {"data": name, "test": test}
+
+    #def post(self):
+    #    return {"data": "posted"}
+
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video is required", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes of the video is required", required=True)
+
+#videos = {}
+
+#def abort_if_video_id_doesnt_exist(video_id):
+#    if video_id not in videos:
+#        #abort(404, message="Could not find video...")
+#        abort(404, "Could not find video...")
+
+#def abort_if_video_exists(video_id):
+#    if video_id in videos:
+#        abort(409, "Video already exists with that ID...")
+
+# seiralize
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer
+}
+
+class Video(Resource):
+    @marshal_with(resource_fields)
+    def get(self, video_id):
+        #abort_if_video_id_doesnt_exist(video_id)
+        #return videos[video_id]
+        #result = VideoModel.query.filter_by(id=video_id).all()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        return result
+
+    @marshal_with(resource_fields)
+    def put(self, video_id):
+        #print(request.form)
+        #print(request.form['likes'])
+        #return {video_id: args}
+        
+        #args = video_put_args.parse_args()
+        #videos[video_id] = args
+        #return videos[video_id], 201
+
+        args = video_put_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
+
+    def delete(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        del videos[video_id]
+        return '', 204
+
+#api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>")
+#api.add_resource(HelloWorld, "/helloworld/<string:name>")
+api.add_resource(Video, "/video/<int:video_id>")
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ test/test.py
@@ -0,0 +1,23 @@
+import requests
+
+BASE = "http://127.0.0.1:5000/"
+
+data = [{"likes": 10, "name": "kindsr", "views": 9900000},
+        {"likes": 78, "name": "Joe", "views": 10000000},
+        {"likes": 10000, "name": "How to make REST API", "views": 80000},
+        {"likes": 35, "name": "Time", "views": 2000},]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+#response = requests.get(BASE + "helloworld/kindsr")
+#response = requests.put(BASE + "video/3", {"likes": 10, "name": "kindsr", "views": 10000000})
+#print(response.json())
+
+#input()
+#response = requests.delete(BASE + "video/0")
+#print(response)
+input()
+response = requests.get(BASE + "video/2")
+print(response.json())
